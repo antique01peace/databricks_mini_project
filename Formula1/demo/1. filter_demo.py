0ab10b0e0b87901419cd769d7189ddead95badb9
antique01peace <antique01peace>
2024-03-28 09:22:51 +0000

    exploring filter

---
+++ Formula1/demo/1. filter_demo.py
@@ -0,0 +1,24 @@
+# Databricks notebook source
+# MAGIC %run "../includes/configuration"
+
+# COMMAND ----------
+
+races_df = spark.read.parquet(f"{processed_folder_path}/races")
+
+# COMMAND ----------
+
+display(races_df)
+
+# COMMAND ----------
+
+races_filtered_df = races_df.where("race_year = 2019 and round <= 5")
+display(races_filtered_df)
+
+# COMMAND ----------
+
+races_filtered_df = races_df.where((races_df["race_year"] == 2019) & (races_df["round"] <= 5))
+display(races_filtered_df)
+
+# COMMAND ----------
+
+
