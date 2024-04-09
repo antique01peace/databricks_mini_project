5e686742b15936f22cf6f664468ff13ef3d988f0
antique01peace <antique01peace>
2024-04-09 08:28:16 +0000

    exploring sql objects in spark

--- Formula1/demo/sql_objects.sql
+++ Formula1/demo/sql_objects.sql
@@ -1,24 +1,13 @@
 -- Databricks notebook source
+-- MAGIC %run "../includes/configuration"
+
+-- COMMAND ----------
+
+-- MAGIC %md ###Explore Managed Tables
+
+-- COMMAND ----------
+
 CREATE DATABASE IF NOT EXISTS demo;
-
--- COMMAND ----------
-
--- MAGIC %python
--- MAGIC spark.read.parquet("/mnt/formula1dl/processed/circuits").write.saveAsTable("demo.circuits")
--- MAGIC
-
--- COMMAND ----------
-
--- MAGIC
--- MAGIC %fs ls "/mnt/formula1dl/processed"
-
--- COMMAND ----------
-
-describe demo.circuits
-
--- COMMAND ----------
-
-USE default;
 
 -- COMMAND ----------
 
@@ -26,21 +15,45 @@ show databases;
 
 -- COMMAND ----------
 
--- MAGIC %run "../includes/configuration" 
+describe database extended demo;
+
+-- COMMAND ----------
+
+select current_database()
+
+-- COMMAND ----------
+
+show tables;
+
+-- COMMAND ----------
+
+select current_catalog()
+
+-- COMMAND ----------
+
+desc catalog extended spark_catalog
+
+-- COMMAND ----------
+
+show catalogs;
+
+-- COMMAND ----------
+
+show tables in demo;
+
+-- COMMAND ----------
+
+use demo;
 
 -- COMMAND ----------
 
 -- MAGIC %python
--- MAGIC race_results_df = spark.read.parquet(f"{presentation_folder_path}/race_results") 
+-- MAGIC spark.read.parquet(f"{presentation_folder_path}/race_results").write.saveAsTable("race_results_python")
+-- MAGIC
 
 -- COMMAND ----------
 
--- MAGIC %fs ls "/mnt/formula1dl"
-
--- COMMAND ----------
-
--- MAGIC %python
--- MAGIC race_results_df.write.saveAsTable("demo.race_results")
+-- MAGIC %fs ls "/user/hive/warehouse"
 
 -- COMMAND ----------
 
@@ -48,6 +61,7 @@ show databases;
 
 -- COMMAND ----------
 
+use default;
 show tables;
 
 -- COMMAND ----------
@@ -57,40 +71,39 @@ show tables;
 
 -- COMMAND ----------
 
-desc extended demo.race_results
+desc extended demo.race_results_python
 
 -- COMMAND ----------
 
-desc extended demo.race_results_python_new
+create table race_results_sql
+as
+select * 
+  from demo.race_results_python
 
 -- COMMAND ----------
 
-drop table demo.race_results_python_new
+select * from race_results_sql
+
+-- COMMAND ----------
+
+drop table demo.race_results_sql
 
 -- COMMAND ----------
 
 -- MAGIC %fs
--- MAGIC ls "dbfs:/user/hive/warehouse/demo.db/"
+-- MAGIC ls "/user/hive/warehouse/demo.db"
 
 -- COMMAND ----------
 
 create table race_results_2020
 as
 select * 
-  from demo.race_results
+  from demo.race_results_python
 where race_year = 2020;
 
 -- COMMAND ----------
 
--- MAGIC %fs ls "dbfs:/user/hive/warehouse/demo.db/"
-
--- COMMAND ----------
-
 desc extended demo.race_results_2020;
-
--- COMMAND ----------
-
-drop table as;
 
 -- COMMAND ----------
 
@@ -100,6 +113,109 @@ drop table demo.race_results_2020;
 
 -- MAGIC %python
 -- MAGIC display(dbutils.fs.ls("/user/hive/warehouse/demo.db"))
+
+-- COMMAND ----------
+
+-- MAGIC %md ###EXTERNAL TABLES
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC race_results_df = spark.read.parquet(f"{presentation_folder_path}/race_results")
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC
+-- MAGIC race_results_df.write.format("parquet").option("path", f"{presentation_folder_path}/race_results_ext").saveAsTable("demo.race_results_ext_py")
+
+-- COMMAND ----------
+
+desc extended demo.race_results_ext_py
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC display(dbutils.fs.ls("dbfs:/mnt/formula1dl/presentation/race_results_ext_sql"))
+
+-- COMMAND ----------
+
+select * from demo.race_results_ext_py
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC race_results_df.printSchema()
+
+-- COMMAND ----------
+
+create table race_results_ext_sql
+(
+  race_year int,
+  race_name string, 
+  race_date timestamp,
+  circuit_location string, 
+  driver_name string,
+  driver_number integer,
+  driver_nationality string,
+  team string,
+  grid integer,
+  fastest_lap integer, 
+  race_time string,
+  points float, 
+  position integer,
+  ingestion_date timestamp
+)
+using parquet
+location "/mnt/formula1dl/presentation/race_results_ext_sql"
+
+-- COMMAND ----------
+
+desc extended race_results_ext_sql;
+
+-- COMMAND ----------
+
+insert into demo.race_results_ext_sql
+ select * from demo.race_results_ext_py
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ####VIEWS
+-- MAGIC 1. Temp Views
+-- MAGIC 2. Global Temp View
+-- MAGIC 3. Permanent View
+
+-- COMMAND ----------
+
+create or replace temp view v_race_results
+as 
+select * 
+  from race_results_python
+  where race_year = 2020
+
+-- COMMAND ----------
+
+select * from v_race_results;
+
+-- COMMAND ----------
+
+create or replace global temp view gv_race_results
+as 
+  select * from race_results_python
+  where race_year = 2018
+
+-- COMMAND ----------
+
+show databases;
+
+-- COMMAND ----------
+
+select * from global_temp.gv_race_results
+
+-- COMMAND ----------
+
+show tables in global_temp;
 
 -- COMMAND ----------
 
